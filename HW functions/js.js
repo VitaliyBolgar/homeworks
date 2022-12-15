1058cc7d72d3617185c1b419bccd954200da928f
orikiri <oriki4610@icloud.com>
2022-12-16 00:38:42 +0300

    update js.js
    Added shorter function explanation

--- HW functions/js.js
+++ HW functions/js.js
@@ -3,41 +3,50 @@
 
 // ==================Вариант 1===================
 
-function calcSum(a,b) {
-    return (alert (`Result: ${(a+b)}`));
-}
-let num1 = +prompt("Введите первое число")
-let num2 = +prompt("Введите второе число")
+// function calcSum(a,b) {
+//     return (alert (`Result: ${(a+b)}`));
+// }
+// let num1 = +prompt("Введите первое число")
+// let num2 = +prompt("Введите второе число")
 
-calcSum(num1, num2)
+// calcSum(num1, num2)
 
 // =================Вариант 3=====================
 
-function calcSum(a,b) {
-    return a+b;
-}
+// function calcSum(a,b) {
+//     return a+b;
+// }
 
-let num1 = +prompt("Введите первое число")
-let num2 = +prompt("Введите второе число")
-let result = calcSum(num1,num2)
+// let num1 = +prompt("Введите первое число")
+// let num2 = +prompt("Введите второе число")
+// let result = calcSum(num1,num2)
 
-alert (result)
+// alert (result)
 
 
 
 //------------------ЗАДАЧА 2-----------------------
 
+// function checkPalindrom(a) {
+//     for (i=0; i<a.length/2; i++) {
+//         return (a[i] == a[a.length-1-i]) ? true : false
+//     }
+
+// }
+
+// let b = prompt("Введите слово для проверки").toLowerCase()
+// if(checkPalindrom(b)) {
+//     alert(`Введенное вами: "${b}" - является палиндромом.`)
+// }
+// else {
+//     alert (`Введенное вами: "${b}" - не является палиндромом.`)
+// }
+
 function checkPalindrom(a) {
     for (i=0; i<a.length/2; i++) {
-        return (a[i] == a[a.length-1-i]) ? true : false
+        return (a[i] == a[a.length-1-i]) ? alert(`${a} - это палиндром`) : alert(`${a} - это не палиндром`)
     }
-
 }
-
-let b = prompt("Введите слово для проверки").toLowerCase()
-if(checkPalindrom(b)) {
-    alert(`Введенное вами: "${b}" - является палиндромом.`)
-}
-else {
-    alert (`Введенное вами: "${b}" - не является палиндромом.`)
-}
+    
+let b = prompt("Введите текст").toLowerCase()
+checkPalindrom(b)
