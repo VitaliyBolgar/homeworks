56908eeb8a56b7960f649d47b541845de7f8c9c8
orikiri <oriki4610@icloud.com>
2022-12-26 15:54:55 +0300

    added homework
    -

---
+++ HW 7/js.js
@@ -0,0 +1,50 @@
+//  Объект
+const company = {
+    head: 'Ivanov',
+    manager: 'Petrov',
+    cars: [
+      { mark: 'Audi', price: 1500000, color: 'white' },
+      { mark: 'BMW', price: 1800000, color: 'white' },
+      { mark: 'Mercedes', price: 2500000, color: 'black' },
+    
+    ],
+    staff: {
+      role: 'cleaner', age: 45, name: 'Ivan'
+    },
+    adress: "Spb, Leninskiy avenue",
+    
+}
+
+// Задача 1:
+// Напишите функцию для удаления переданного в параметры ключа из переданного объекта.
+
+delete company.manager 
+console.log(company);
+
+// Задача 2
+// Напишите функцию, которая принимает объект и возвращает его длину
+
+//-------------метод Object.keys
+console.log(Object.keys(company).length); // - object.keys возвращает массив ключей. В скобках указываем имя массива, далее через точку length выводим длину
+
+
+//-------------перебор
+let a = 0;
+for (let key in company) {
+    a++
+} 
+console.log(a);
+
+
+// Задача 3
+// Напишите функцию которая принимает url а возвращает данные с сервера
+
+const myPokemon = async() => { 
+  let pokemons = await fetch('https://pokeapi.co/api/v2/pokemon/charmander') 
+  pokemons = await pokemons.json()
+  console.log(pokemons);
+  console.log(pokemons.name); // - просто имя покемона
+}
+
+myPokemon()
+
